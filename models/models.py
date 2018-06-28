1de7f2c332e3f4df515015e2989f386ccc28b963
unknown <liqiyi@ppdai.com>
2018-06-28 15:39:47 +0800

    can run on pytorch0.2.0

--- models/models.py
+++ models/models.py
@@ -4,11 +4,11 @@ def create_model(opt):
     print(opt.model)
     if opt.model == 'cycle_gan':
         from .cycle_gan_model import CycleGANModel
-        assert(opt.align_data == False)
+        assert (opt.dataset_mode == 'unaligned')
         model = CycleGANModel()
     if opt.model == 'pix2pix':
         from .pix2pix_model import Pix2PixModel
-        assert(opt.align_data == True)
+        assert (opt.dataset_mode == 'aligned')
         model = Pix2PixModel()
     model.initialize(opt)
     print("model [%s] was created" % (model.name()))
